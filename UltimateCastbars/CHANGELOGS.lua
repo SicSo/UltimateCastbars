97f6e1c51e2e5924136a94200839f9b60be93ad0
Sicso <bilcdanielal@yahoo.com>
2026-02-23 23:00:27 +0000

    Revert "chore(release): 0.9.1"
    This reverts commit d5cd4f7eac18d6ccce6954630d730e50f8388dda.

--- UltimateCastbars/CHANGELOGS.lua
+++ UltimateCastbars/CHANGELOGS.lua
@@ -1,8 +1,16 @@
 local _, UCB = ...
 
 UCB.CHANGELOG_TEXT = [=[
+## Version 0.9.2 - [23-02-2026]
+
+### Fixed
+- Fixed automatic release for Curse and Wago attempt 1
+
+---
+
 ## Version 0.9.1 - [23-02-2026]
 
+### Added
 - Support for automatic Curse and Wago releases
 
 ---
