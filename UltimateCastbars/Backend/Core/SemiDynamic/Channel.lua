cd7619f5943420ea589d713b80b7b530ba031118
Sicso <bilcdanielal@yahoo.com>
2026-02-26 04:54:30 +0000

    Removed debug print statements

--- UltimateCastbars/Backend/Core/SemiDynamic/Channel.lua
+++ UltimateCastbars/Backend/Core/SemiDynamic/Channel.lua
@@ -174,9 +174,7 @@ function CASTBAR_API:OnUnitSpellcastChannelStart(unit, castGUID, spellID, castBa
 end
 
 function CASTBAR_API:OnUnitSpellcastChannelUpdate(unit, castGUID, spellID, castBarID)
-    print("UPDATE")
     local show = CASTBAR_API:CastUpdate(unit, castGUID, spellID, castType)
-    print(show)
     if not show then return end
     CASTBAR_API:AssignChannelTicks(unit, spellID, "UPDATE")
 end
